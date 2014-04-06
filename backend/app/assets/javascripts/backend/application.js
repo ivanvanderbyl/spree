d7c2831abaffea6162a173b6f1da24ab92a38248
Ryan Bigg <radarlistener@gmail.com>
2014-04-07 09:24:32 +1000

    move formatting helpers into app/assets/javascripts/backend/helpers

--- backend/app/assets/javascripts/backend/application.js
+++ backend/app/assets/javascripts/backend/application.js
@@ -35,19 +35,3 @@ Backend = Ember.Application.create({
   }),
   rootElement: '#orders'
 });
-
-Ember.Handlebars.helper("prettyDate", function(date) {
-  return moment(date).format(I18n.t('date.format'))
-})
-
-Ember.Handlebars.registerHelper('i18n', function(property, options) {
-  var params = options.hash,
-  self = this;
-
-  // Support variable interpolation for our string
-  Object.keys(params).forEach(function (key) {
-    params[key] = Em.Handlebars.get(self, params[key], options);
-  });
-
-  return I18n.t(property, params);
-});
